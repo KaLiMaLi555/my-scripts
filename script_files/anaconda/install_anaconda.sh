49ea20954f150e55a1b30b8804d6c44f611dc17e
Kirtan Mali <43683545+KaLiMaLi555@users.noreply.github.com>
2020-07-24 02:45:16 -0400

    Update install_anaconda.sh

--- script_files/anaconda/install_anaconda.sh
+++ script_files/anaconda/install_anaconda.sh
@@ -11,7 +11,7 @@ install_anaconda()
 {
     sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
     wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
-    bash ~/Downloads/Anaconda3-2020.02-Linux-x86_64.sh
+    bash Anaconda3-2020.02-Linux-x86_64.sh
 }
 
 install_anaconda
